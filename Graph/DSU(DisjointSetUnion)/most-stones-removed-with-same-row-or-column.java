a3e7b4fbeb557fe82dbfee5152a99be9b07de678
coder-era-begins <leetcvishnu@gmail.com>
2025-10-20 19:03:01 +0530

    Create most-stones-removed-with-same-row-or-column.java

---
+++ Graph/DSU(DisjointSetUnion)/most-stones-removed-with-same-row-or-column.java
@@ -0,0 +1,73 @@
+class DisjointSet {
+    List<Integer> rank = new ArrayList<>();
+    List<Integer> parent = new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
+    public DisjointSet(int n) {
+        for (int i = 0; i <= n; i++) {
+            rank.add(0);
+            parent.add(i);
+            size.add(1);
+        }
+    }
+
+    public int findUPar(int node) {
+        if (node == parent.get(node)) {
+            return node;
+        }
+        int ulp = findUPar(parent.get(node));
+        parent.set(node, ulp);
+        return parent.get(node);
+    }
+
+    public void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank.get(ulp_u) < rank.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v);
+        } else if (rank.get(ulp_v) < rank.get(ulp_u)) {
+            parent.set(ulp_v, ulp_u);
+        } else {
+            parent.set(ulp_v, ulp_u);
+            int rankU = rank.get(ulp_u);
+            rank.set(ulp_u, rankU + 1);
+        }
+    }
+
+    public void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size.get(ulp_u) < size.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v);
+            size.set(ulp_v, size.get(ulp_v) + size.get(ulp_u));
+        } else {
+            parent.set(ulp_v, ulp_u);
+            size.set(ulp_u, size.get(ulp_u) + size.get(ulp_v));
+        }
+    }
+}
+class Solution {
+    public int removeStones(int[][] stones) {
+        int maxrow=0;
+        int maxcol=0;
+        for(int stone[]:stones){
+            maxrow=Math.max(maxrow,stone[0]);
+            maxcol=Math.max(maxcol,stone[1]);
+        }
+        DisjointSet ds=new DisjointSet(maxrow+maxcol+1);
+        Set<Integer> stoneSet=new HashSet<>();
+        for(int stone[]:stones){
+            ds.unionByRank(stone[0],stone[1]+maxrow+1);
+            stoneSet.add(stone[0]);
+            stoneSet.add(stone[1]+maxrow+1);
+        }
+        int count=0;
+        for(int node:stoneSet){
+            if(ds.findUPar(node)==node){
+                count++;
+            }
+        }
+        return stones.length - count;
+    }
+}
